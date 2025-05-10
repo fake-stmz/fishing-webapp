ee07650bf6a2787fb15d1469ba5e52cd2f70dce9
fake-stmz <artur.kravchenko.2006@gmail.com>
2025-05-10 11:01:41 +0300

    created a model for sale items

--- fishing_webapp/fisher_haven/models.py
+++ fishing_webapp/fisher_haven/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class SaleItem(models.Model): # Товар на продажу
+    name = models.CharField(max_length=100) # Название товара
+    price = models.DecimalField(max_digits=10, decimal_places=2) # Цена товара
+    quantity = models.IntegerField() # Количество товара в наличии
+    description = models.TextField() # Описание товара
+    image_url = models.TextField() # URL изображения товара

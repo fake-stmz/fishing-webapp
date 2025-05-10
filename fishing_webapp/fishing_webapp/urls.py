297359c3669aab3df7ad480d0c73e45e45a1cf86
fake-stmz <artur.kravchenko.2006@gmail.com>
2025-05-10 11:30:55 +0300

    index page finished

--- fishing_webapp/fishing_webapp/urls.py
+++ fishing_webapp/fishing_webapp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from fisher_haven import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('catalogue/', views.catalogue, name='catalogue'),
 ]
